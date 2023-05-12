3d763f073608a27d38e19d4706f6bb7e76049951
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-12 16:09:38 -0300

    Conceitos iniciais sobre making decision em JS

---
+++ JS_Decision_Making/Conditionals/app.js
@@ -0,0 +1,10 @@
+console.log("Before conditional")
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Congrats! Your number is less than 0.5")
+    console.log(random);
+}
+if (random >= 0.5) {
+    console.log("Congrats! Your number is greater than 0.5!")
+    console.log(random);
+}
