6fbc9cc11c0f683b16695093e36f5de92f0a2767
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-13 10:55:31 -0300

    Uso do if else if else em javascript

--- JS_Decision_Making/Conditionals/app.js
+++ JS_Decision_Making/Conditionals/app.js
@@ -1,10 +1,32 @@
-console.log("Before conditional")
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Congrats! Your number is less than 0.5")
-    console.log(random);
-}
-if (random >= 0.5) {
-    console.log("Congrats! Your number is greater than 0.5!")
-    console.log(random);
+// console.log("Before conditional")
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Congrats! Your number is less than 0.5")
+//     console.log(random);
+// }
+// if (random >= 0.5) {
+//     console.log("Congrats! Your number is greater than 0.5!")
+//     console.log(random);
+// }
+
+// const dayOfWeek = "Friday";
+
+// if (dayOfWeek === "Monday") {
+//     console.log("UGHH I hate mondays!")
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("Yaaaay I LUV saturdays!")
+// } else if (dayOfWeek === "Friday") {
+//     console.log("Friday is a good day")
+// }
+
+const age = 66;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free")
+} else if (age < 10) {
+    console.log ("You are a child. You pay $10")
+} else if (age < 65) {
+    console.log("You are a adult. You pay $20")
+} else {
+    console.log("You are a senior. You pay $10")
 }
