642a75e469a761c7eb5cd670fc8706fc153e6334
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-13 12:23:09 -0300

    Uso do operador lógico NOT em JS

--- JS_Decision_Making/logical_operators/app.js
+++ JS_Decision_Making/logical_operators/app.js
@@ -6,14 +6,24 @@
 //     console.log("Incorrent format for password!")
 // }
 
-const age = -1;
+// const age = -1;
 
-if (age >= 0 && age < 5 || age >65) {
-    console.log("Free");
-} else if (age >= 5 && age < 10) {
-    console.log("$10")
-} else if (age >= 10 && age < 65) {
-    console.log("$20")
-} else {
-    console.log("Invalid Age!")
+// if (age >= 0 && age < 5 || age >65) {
+//     console.log("Free");
+// } else if (age >= 5 && age < 10) {
+//     console.log("$10")
+// } else if (age >= 10 && age < 65) {
+//     console.log("$20")
+// } else {
+//     console.log("Invalid Age!")
+// }
+
+// let firstName = prompt("enter your first name");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN!");
+// }
+
+const age = 10;
+if (!(age >= 0 && age < 5 || age >65)) {
+console.log("You are not a baby or a senior");
 }
