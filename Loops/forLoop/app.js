fa7170df028b6ead8ef58b30b050008df90913cd
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-15 16:02:24 -0300

    Uso de nested loops em JS

--- Loops/forLoop/app.js
+++ Loops/forLoop/app.js
@@ -1,3 +1,23 @@
-for (let i = 1; i <=10; i++) {
-    console.log(i);
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i += 1) {
+//     console.log(i)
+// }
+
+// for (i = 100; i
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++) {
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}`)
+    for (let j = 0; j < row.length; j++) {
+        console.log(row[j])
+    }
 }
