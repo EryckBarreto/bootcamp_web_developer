fe15d8cc684e3d8fef1aaeae5e2691789d8ddca3
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-16 10:28:48 -0300

    Uso do while e do break em JS

---
+++ Loops/whileLoop/app.js
@@ -0,0 +1,14 @@
+// let count = 0;
+// while (count < 10) {
+//     count++;
+//     console.log(count)
+// }
+
+let input = prompt("Hey, say something!")
+while (true) {
+    input = prompt(input);
+    if(input.toLowerCase() === "stop copying me") break;
+    }
+}
+
+console.log("OK YOU WIN!")
