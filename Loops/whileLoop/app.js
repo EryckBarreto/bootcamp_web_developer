4831733e7f37921ff6912ab8a68b820c85ad1d91
Eryck Barreto <eryck.barreto@hotmail.com>
2023-05-16 13:21:47 -0300

    Realizada melhorias para eliminar bugs na execução do algoritmo

--- Loops/whileLoop/app.js
+++ Loops/whileLoop/app.js
@@ -21,14 +21,18 @@ while (!maximum) {
 const targetNum = Math.floor(Math.random() * maximum) + 1
 let attempts = 1;
 
-let guess = parseInt(prompt("Enter your first guess!"));
+let guess = prompt("Enter your first guess! (Type 'q' to quit the game.");
 while (parseInt(guess) !== targetNum) {
     if (guess === 'q') break;
-    attempts++;
+    guess = parseInt(guess);
     if (guess > targetNum) {
         guess = prompt("Too high! Enter a new guess:");
-    } else {
+        attempts++;
+    } else if (guess < targetNum) {
         guess = prompt("Too low! Enter a new guess:");
+        attempts++;
+    } else {
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit");
     }
 }
 
