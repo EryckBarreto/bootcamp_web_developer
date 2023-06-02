ca522b7d394030768d811a7d572fe871555d4aff
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-02 15:58:21 -0300

    alterado o delete colocando o parseInt

--- todoDemo/app.js
+++ todoDemo/app.js
@@ -1,12 +1,13 @@
 
 let input = prompt("What would like to do?");
-let todos = [];
+const todos = [];
 
 while (input !== "quit" && input !== "q") {
 
     if (input === "add") {
         let addTodo = prompt("What do you like to add?")
         todos.push(addTodo);
+        console.log(`${addTodo} add to the list!`)
 
     } else if (input === "list") {
         for (let i = 0; i < todos.length; i++) {
@@ -14,8 +15,7 @@ while (input !== "quit" && input !== "q") {
         }
 
     } else if (input === "delete") {
-        let deleteTodo = prompt("What item would you like to delete? Insert the number.");
-        
+        const deleteTodo = parseInt(prompt("What item would you like to delete? Insert the number."));
         if(deleteTodo >= 0 && delete Todo < todos.length) {
             todos.splice(deleteTodo, 1);
         } else {
