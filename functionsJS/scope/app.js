7ab6560624efef0ed8980d8021beb7436844f08c
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-04 13:45:26 -0300

    lexical scope em JS

--- functionsJS/scope/app.js
+++ functionsJS/scope/app.js
@@ -7,3 +7,25 @@ function collectEggs() {
 // em razão do escopo da função, a variável está dentro
 // da função
 
+let radius = 8;
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = "Hiii!"
+}
+
+// console.log(radius);
+// console.log(PI);
+
+// /\ block scope, variável não acessa
+
+// lexical scope
+
+function bankRobbery() {
+    const heroes = ["Spiderman", "Wolverine", "Black Panther"];
+    function cryForHelp() {
+        for (let hero of heroes) {
+            console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+        }
+    }
+    cryForHelp();
+}
