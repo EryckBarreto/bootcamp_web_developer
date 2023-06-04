e247a0aac6f051e0185abf37e07477f839c556d6
EryckBarreto <eryck.barreto@hotmail.com>
2023-06-04 13:05:27 -0300

    Uso do scope em JS

---
+++ functionsJS/scope/app.js
@@ -0,0 +1,9 @@
+function collectEggs() {
+    let totalEggs = 6;
+    console.log(totalEggs);
+}
+
+// não conseguimos acessar a variável dentro da função
+// em razão do escopo da função, a variável está dentro
+// da função
+
